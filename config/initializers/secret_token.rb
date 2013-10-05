e5eb46ce8696d8163bc68a72a3bad721a2379059
Artiwarah Damrongchai <artiwarahd@gmail.com>
2013-10-05 23:34:59 +0700

    #57664264
    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FurnitureStore::Application.config.secret_token = 'd644275f234bddab8101b786755faea2b7bb00d6df0badadda65c0b86d60ee35eb9b76119ae8c0d2ebe05e542614a4ee0c6b5714a88efe683de7243b1b5da74e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FurnitureStore::Application.config.session_store :cookie_store, key: '_furniture_store_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FurnitureStore::Application.config.session_store :active_record_store
