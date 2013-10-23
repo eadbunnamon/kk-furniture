e537cc36734dcf1892ecc9241b857b26f2b1d148
Artiwarah Damrongchai <artiwarahd@gmail.com>
2013-10-23 21:06:43 +0700

    Search products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class ProductsController < ApplicationController
 
   def show
@@ -28,5 +29,39 @@ class ProductsController < ApplicationController
     current_id = @product_ids[current_index]
     @next_id = current_id == @product_ids.last ? current_id : @product_ids[current_index + 1]
     @prev_id = current_id == @product_ids.first ? current_id : @product_ids[current_index - 1]
+  end
+
+  def search
+    keywords = params[:keywords].downcase.split(/\s/)
+    @original_keywords = params[:keywords]
+
+    result = {}
+    product_conditions = []
+    room_conditions = []
+    category_conditions = []
+
+    keywords.each do |keyword|
+      product_conditions << "LOWER(products.name) LIKE '%#{keyword}%'"
+      product_conditions << "LOWER(products.detail) LIKE '%#{keyword}%'"
+      product_conditions << "LOWER(products.description) LIKE '%#{keyword}%'"
+      product_conditions << "LOWER(products.price) LIKE '%#{keyword}%'"
+
+      room_conditions << "LOWER(rooms.name) LIKE '%#{keyword}%'"
+      room_conditions << "LOWER(rooms.description) LIKE '%#{keyword}%'"
+
+      category_conditions << "LOWER(categories.name) LIKE '%#{keyword}%'"
+      category_conditions << "LOWER(categories.description) LIKE '%#{keyword}%'"
+    end
+
+    @products = Product.where(product_conditions.join(" OR "))
+
+    products_from_categories = Category.where(category_conditions.join(" OR ")).map(&:products)
+    products_from_rooms = Room.where(room_conditions.join(" OR ")).map(&:products)
+
+    (products_from_categories | products_from_rooms).each do |products_array|
+      @products << products_array
+    end
+
+    @products = @products.uniq
   end
 end
