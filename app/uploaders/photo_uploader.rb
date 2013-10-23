76fbf20016e682393b6c3faca117f287fe0cf0a5
Artiwarah Damrongchai <artiwarahd@gmail.com>
2013-10-23 22:50:05 +0700

    resize thumb photo

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -33,7 +33,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [156, 156]
+    process :resize_to_limit => [0, 156]
+    # process :resize_to_fill => [156, 156]
+    # process :resize_to_fit => [156, 156]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
