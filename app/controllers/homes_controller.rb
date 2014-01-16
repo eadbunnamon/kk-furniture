7b74fa8f900ba59da8e2eaf4592c3ce52d36818e
Artiwarah Damrongchai <artiwarahd@gmail.com>
2014-01-16 19:18:07 +0700

    fine tune UI

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,10 @@
 class HomesController < ApplicationController
   def index
     @best_sellers = Product.where(best_seller: true)
-    @new_arrivals = Product.order("created_at desc")
+    @new_arrivals = Product.order("created_at desc").limit(5)
     @favorites = Product.order("view desc").limit(5)
 
-    @new_arrival_photo = @new_arrivals.last.photos.first.photo.thumb.url if @new_arrivals.present? && @new_arrivals.last.photos.present?
+    @new_arrival_photo = @new_arrivals.first.photos.first.photo.thumb.url if @new_arrivals.present? && @new_arrivals.last.photos.present?
     @best_seller_photo = @best_sellers.last.photos.first.photo.thumb.url if @best_sellers.present? && @best_sellers.last.photos.present?
     @promotion_photo = Promotion.last.file.thumb.url if @promotion_photo.present?
   end
